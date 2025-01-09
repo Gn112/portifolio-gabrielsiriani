9b3a6c66f5ca0458aa3edb5c8641c3bb70b81bee
Gn112 <gabrielsiriani19@gmail.com>
2025-01-09 19:00:57 -0300

    pause.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,23 @@ function App() {
   return (
     <>
       <Header />
+
+      <section className="container">
+        <div className="apresentacao">
+          <p>
+            Olá, sou <br/>
+            <span>Gabriel Siriani</span> <br/>
+            Desenvolvedor Full-Stack
+          </p>
+          <button className='btn btn-red'>
+            Saiba mais sobre mim
+          </button>
+        </div>
+        <figure>
+          <img src="src/assets/web_developer.svg" alt="Imagem da home" className="img-home" />
+        </figure>
+      </section>
+
       <Footer />
     </>
   )
